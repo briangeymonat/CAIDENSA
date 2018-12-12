1d7cdd7268cd697d5ab6f193ecc1be9c51d99126
DESKTOP-20O7FSI\Sebastian <sebamu98@hotmail.com>
2018-12-12 16:30:23 -0300

    Corrección de la iteración 2

--- Programa1/Ejemplo.Web/Dominio/dFachada.cs
+++ Programa1/Ejemplo.Web/Dominio/dFachada.cs
@@ -142,6 +142,10 @@ namespace Dominio
         {
             return dBeneficiario.TraerEspecificoCI(parBeneficiario);
         }
+        public static cBeneficiario BeneficiarioTraerEspecificoVerificarModificar(cBeneficiario parBeneficiario)
+        {
+            return dBeneficiario.TraerEspecificoVerificarModificar(parBeneficiario);
+        }
         public static List<cBeneficiario> BeneficiarioTraerTodos()
         {
             return dBeneficiario.TraerTodos();
